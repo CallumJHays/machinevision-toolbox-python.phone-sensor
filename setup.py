aea80b19d4e162de576411a6e8e868bc2f6b8621
CallumJHays <callumjhays@gmail.com>
2021-03-02 12:58:34 +1000

    bump ver 0.2.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     
     name='machinevision-toolbox-python.phone-sensor', 
 
-    version="0.1.0",
+    version="0.2.1",
 
     description='PhoneSensor for machinevisiontoolbox. Get camera and IMU data from a camera remotely with Python',
     

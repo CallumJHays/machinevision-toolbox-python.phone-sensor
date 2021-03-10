5528ece28570fe98f945ce5c1dbd94cddd76c2ad
GH Actions CI <callumjhays@gmail.com>
2021-03-10 01:53:37 +0000

    Bump version: v0.2.11 → 0.2.12

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 
     name='machinevision-toolbox-python.phone-sensor',
 
-    version="0.2.11",
+    version="0.2.12",
 
     description='PhoneSensor for machinevisiontoolbox. Get camera and IMU data from a camera remotely with Python',
 
